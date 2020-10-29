bf89e16953183c6e9edfb9bd6be15f3346728b37
Njaal Wiik <njaal.wiik@gmail.com>
2020-10-29 13:10:05 +0100

    Minor updates

--- src/portfolio.js
+++ src/portfolio.js
@@ -552,7 +552,7 @@ const experience = {
           duration: "Aug 2017 - May 2020",
           location: "Oslo, Norway",
           description:
-            "Presenter for BI. Doing lectures. pitches and sales talks for potential customers. Followed up prospects with a 10% conversion rate and managed social media channels.",
+            "Presenter for BI. Doing lectures, pitches and sales talks for potential customers. Followed up prospects with a 10% conversion rate and managed social media channels.",
           color: "#056CB4",
         },
       ],
